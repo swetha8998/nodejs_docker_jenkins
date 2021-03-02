eb7dfcce434fa926ffe49a2bfff260ba74e534d2
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-03-02 15:28:26 +0530

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,28 @@
+pipeline{
+ agent {
+    dockerfile true
+  }
+stages{
+stage('git'){
+  steps{
+git branch: 'master', url: 'https://github.com/swetha8998/nodejs_docker_jenkins.git'
+  }
+}
+stage('Build'){
+  steps{
+sh 'echo "in building stage"'
+  }
+}
+stage('Approval'){
+  steps{
+sh 'echo "in approval stage" '
+
+  }
+}
+stage('Deploy'){
+  steps{
+sh 'echo "in deployment stage" '
+ }
+}
+ }
+ 
