af6070066db93b1f2155617b8ba62981e5c68e62
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-06-17 17:39:26 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,3 +1,7 @@
+final STACKNAME= 'Hello-World-node'
+final ACCOUNT= '993745358053'
+final ECR_REGISTRY='993745358053.dkr.ecr.us-east-1.amazonaws.com'
+version = currentBuild.number
 pipeline{
 agent any
 
@@ -13,6 +17,10 @@ stage('Build'){
 
    
    sh 'docker build  . -f Dockerfile -t swet/nodeapp'
+     sh 'aws ecr-public create-repository --repository-name ${STACKNAME}'
+     sh 'docker tag ${STACKNAME}:latest ${ECR_REGISTRY}/${STACKNAME}'
+     sh 'docker push ${ECR_REGISTRY}/${STACKNAME}:${version}'
+     sh 'echo "image is pushed" '
    }
   }
 }
